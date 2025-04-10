3a2b845d6d56d9454da4728707f251255b9a051f
deukr <deukryeongoh@gmail.com>
2025-04-10 18:23:46 +0900

    [feat] 회원가입, 개인정보 등록 로직, DTO 수정, Security 설정 추가

--- src/main/java/selfit/selfit/global/exception/MapperException.java
+++ src/main/java/selfit/selfit/global/exception/MapperException.java
@@ -5,6 +5,6 @@ import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
-public class MapperException extends Exception {
+public class MapperException extends RuntimeException {
     private final ErrorCode errorCode;
 }
